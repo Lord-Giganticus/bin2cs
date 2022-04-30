080dedeea114c2eb4e0f988a1263ecae964be054
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-04-30 15:40:18 -0600

    Add project files.

---
+++ bin2cs/Program.cs
@@ -0,0 +1,13 @@
+﻿global using System.Text;
+using bin2cs;
+
+foreach (var arg in args)
+{
+    if (File.Exists(arg) && !Directory.Exists(arg))
+    {
+        Template t = new FileInfo(arg);
+        (Span<byte> span, string name) = t.Process();
+        using var f = File.Create(name);
+        f.Write(span);
+    }
+}
